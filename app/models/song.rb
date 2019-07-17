da7252dfd825ad69ee794584c3e3892b5f001b4e
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-17 00:42:34 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,6 +8,6 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
-    Artist.find_or_create()
+    Artist.find_or_create(name: "Drake")
   end
 end
